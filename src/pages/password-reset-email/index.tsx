d8cd25a9f3b8b26e2a5dc826edd7c35d288b0754
ihamzanaeem <hamza.naeem@cloudmeshsolutions.com>
2023-09-22 19:35:37 +0500

    login and registration flow structured done

--- src/pages/password-reset-email/index.tsx
+++ src/pages/password-reset-email/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Index = () => {
   return (
-     <div className="flex flex-col justify-center min-h-screen">
+      <div className="flex flex-col justify-center min-h-screen">
       <div className="mx-auto max-w-[1030px]    shadow-loginCard ">
         <div className="p-[14px] flex min-h-[749px] ">
            <ForgetPasswordEmail />
