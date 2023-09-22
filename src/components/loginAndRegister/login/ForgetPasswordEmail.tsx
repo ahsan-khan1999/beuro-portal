3cb69c99e350006c52ed54fc5de2af15c90dedf8
ihamzanaeem <hamza.naeem@cloudmeshsolutions.com>
2023-09-22 19:35:21 +0500

    login and registration flow structure done

---
+++ src/components/loginAndRegister/login/ForgetPasswordEmail.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "@/assets/svgs/logo.svg";
+import Image from "next/image";
+
+import EntryLinks from "../EntryLinks";
+import { Form } from "@/base-components/form/form";
+import useFrogetPassword from "@/hooks/auth/useFrogetPassword";
+
+const ForgetPasswordEmail = () => {
+  const defaultClassName = "mt-[30px]  ";
+  const { fields, onSubmit, handleSubmit, errors, error } = useFrogetPassword();
+
+  return (
+    <div className="w-[50%] px-7 flex flex-col justify-between">
+      <Image src={logo} alt="Buro Logo" className="mt-5" />
+      <div className="px-[32px] flex flex-col justify-center pt-3 ">
+        <div className=" max-w-[384px] w-full  ">
+          <h1 className="font-semibold text-dark text-[26px] text-center">
+            Forget Password
+          </h1>
+          <span className="mt-3 text-xs text-[#8F8F8F] flex justify-center">
+            Manage your business with us
+          </span>
+
+          <Form
+            formFields={fields}
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit}
+            errors={errors}
+            className={`${defaultClassName}`}
+          />
+        </div>
+      </div>
+      <EntryLinks />
+    </div>
+  );
+};
+
+export default ForgetPasswordEmail;
---
+++ src/pages/reset-password/index.tsx
@@ -0,0 +1,18 @@
+import DescriptionSection from "@/components/loginAndRegister/DescriptionSection";
+import ResetPassword from "@/components/loginAndRegister/login/ResetPassword";
+import React from "react";
+
+const Index = () => {
+  return (
+    <div className="flex flex-col justify-center min-h-screen">
+      <div className="mx-auto max-w-[1030px]    shadow-loginCard ">
+        <div className="p-[14px] flex min-h-[749px] ">
+          <ResetPassword />
+          <DescriptionSection />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
