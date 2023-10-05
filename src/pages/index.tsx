f355b20cb6beeba115caf7e7d01ff0c950fcbc8d
Ahsan Khan <ahsan.ak943@gmail.com>
2023-10-05 02:38:59 +0000

    filter updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Table from "@/base-components/table";
+// import Table from "@/base-components/table";
 
 export default function Home() {
-  return <Table />;
+  return <></>;
 }
