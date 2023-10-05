e80882f4fdd870c874750dfa6f25615ffd747bde
ihamzanaeem <hamza.naeem@cloudmeshsolutions.com>
2023-10-05 21:33:38 +0500

    customer details form in progress

--- src/components/customer/index.tsx
+++ src/components/customer/index.tsx
@@ -9,22 +9,29 @@ const CustomerDetails = () => {
     useCustomerDetails();
   return (
     <Layout>
-      <div className="rounded-md bg-white pt-5 px-6 pb-6 border border-primary">
-        <div className="flex justify-between items-center pb-5 border-b border-black border-opacity-20">
-          <h2 className="text-[#393939] text-lg font-medium">
+      <div className="flex">
+        <div className="rounded-md bg-white pt-5 px-6 pb-6 border border-primary w-full h-fit">
+          <div className="flex justify-between items-center pb-5 border-b border-black border-opacity-20">
+            <h2 className="text-[#393939] text-lg font-medium">
+              Customer Details
+            </h2>
+            <button className="text-[#4B4B4B] font-medium rounded-lg border border-[#C7C7C7] py-[7px] px-9">
+              Cancel
+            </button>
+          </div>
+          <Form
+            formFields={fields}
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit}
+            errors={errors}
+            className={`${defaultClassName}`}
+          />
+        </div>
+        <div className="ml-8 bg-white rounded-md px-5 py-6 w-full max-w-[254px] h-[634px]">
+          <h2 className="text-[#393939] text-lg font-medium pb-6 border-b border-black border-opacity-20">
             Customer Details
           </h2>
-          <button className="text-[#4B4B4B] font-medium rounded-lg border border-[#C7C7C7] py-[7px] px-9">
-            Cancel
-          </button>
         </div>
-        <Form
-          formFields={fields}
-          handleSubmit={handleSubmit}
-          onSubmit={onSubmit}
-          errors={errors}
-          className={`${defaultClassName}`}
-        />
       </div>
     </Layout>
   );
