6e9e96f32f0a95a1510ff74143b8719daa23b3bc
ihamzanaeem <hamza.naeem@cloudmeshsolutions.com>
2023-11-10 22:00:54 +0500

    super admin dashboard flow in progress

---
+++ src/components/admin/dashboard/filters.tsx
@@ -0,0 +1,28 @@
+import SelectField from "@/base-components/filter/fields/select-field";
+import React, { useState } from "react";
+
+export default function DashboardFilters() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="flex space-x-4">
+      <SelectField
+        handleChange={(value) => console.log(value)}
+        value=""
+        dropDownIconClassName=""
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        options={["1", "2", "2", "2", "2"]}
+        label="Monthly"
+      />
+      <SelectField
+        handleChange={(value) => console.log(value)}
+        value=""
+        dropDownIconClassName=""
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        options={["1", "2", "2", "2", "2"]}
+        label="February"
+      />
+    </div>
+  );
+}
