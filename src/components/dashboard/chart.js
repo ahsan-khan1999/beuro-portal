76e454e49d167f10291b8065144f3a10ab39fdd5
ihamzanaeem <hamza.naeem@cloudmeshsolutions.com>
2023-11-16 22:11:35 +0500

    set styling of legend labels of chart of customer dashboard

--- src/components/dashboard/chart.js
+++ src/components/dashboard/chart.js
@@ -15,16 +15,29 @@ const PieChart = ({ data }) => {
         plugins: {
           legend: {
             position: "bottom",
+            maxWidth: "10px",
+            labels: {
+              // color:"#fff",
+
+              boxWidth: 15,
+              // padding:"0px"
+              // padding: {
+              //   top:20
+              // },
+            },
           },
         },
       },
     });
-
+    document
+      .querySelector(".chart-legend-custom-padding")
+      ?.classList.add("chart-legend-custom-padding");
     // Clean up the chart when the component is unmounted
     return () => {
       myPieChart.destroy();
     };
   }, [data]);
+
 
   return (
     <div className="rounded-[20px] bg-white max-w-[400px]">
@@ -32,7 +45,7 @@ const PieChart = ({ data }) => {
       <hr className="h-[1px] text-black opacity-10 pb-11" />
       <div className="px-6 pb-5">
         <div className="max-w-[300px] mx-auto ">
-          <canvas id="myPieChart" width="300" height="300" />
+          <canvas className="asd" id="myPieChart" width="300" height="300" />
         </div>
       </div>
     </div>
