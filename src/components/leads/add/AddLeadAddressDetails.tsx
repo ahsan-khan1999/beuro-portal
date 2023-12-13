28ec396e53111aa034f48bb1bf3da46e9f9825bf
Mateen Nawaz <mateen.nawaz@cloudmeshsolutions.com>
2023-12-13 16:54:41 +0000

    leads add flow refactor

--- src/components/leads/add/AddLeadAddressDetails.tsx
+++ src/components/leads/add/AddLeadAddressDetails.tsx
@@ -5,17 +5,31 @@ import React from "react";
 import { useRouter } from "next/router";
 import { ComponentsType } from "./AddNewLeadsData";
 
-const AddLeadAddressDetails = ({ onHandleBack,onHandleNext }: { onHandleBack: (currentComponent: ComponentsType) => void, onHandleNext: (currentComponent: ComponentsType) => void }) => {
+const AddLeadAddressDetails = ({
+  onHandleBack,
+  onHandleNext,
+}: {
+  onHandleBack: (currentComponent: ComponentsType) => void;
+  onHandleNext: (currentComponent: ComponentsType) => void;
+}) => {
   const defaultClassName = "";
-  const { fields, control, onSubmit, handleSubmit, errors,translate } =
-    useAddLeadAddressDetails(onHandleBack,onHandleNext);
-  const router = useRouter()
+  const { fields, control, onSubmit, handleSubmit, errors, translate } =
+    useAddLeadAddressDetails(onHandleBack, onHandleNext);
+  const router = useRouter();
 
   return (
     <FormCard>
-      <div className="flex justify-between items-center pb-5 " id="Address Details">
-        <h2 className="text-[#393939] text-lg font-medium">{translate("leads.address_details.main_heading")}</h2>
-        <button onClick={() => router.push("/leads")} className="text-[#4B4B4B] font-medium rounded-lg border border-[#C7C7C7] py-[7px] px-4 max-w-[131px] w-full">
+      <div
+        className="flex justify-between items-center pb-5 "
+        id="Address Details"
+      >
+        <h2 className="text-[#393939] text-lg font-medium">
+          {translate("leads.address_details.main_heading")}
+        </h2>
+        <button
+          onClick={() => router.push("/leads")}
+          className="text-[#4B4B4B] font-medium rounded-lg border border-[#C7C7C7] py-[7px] px-4 max-w-[131px] w-full"
+        >
           {translate("leads.address_details.cancel_button")}
         </button>
       </div>
@@ -29,7 +43,6 @@ const AddLeadAddressDetails = ({ onHandleBack,onHandleNext }: { onHandleBack: (c
         errors={errors}
         className={`${defaultClassName}`}
       />
-
     </FormCard>
   );
 };
--- src/hooks/leads/useAddNewLeadCustomer.tsx
+++ src/hooks/leads/useAddNewLeadCustomer.tsx
@@ -1,14 +1,22 @@
 import { loginUser } from "@/api/slices/authSlice/auth";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { FieldValues, SubmitHandler, UseFormRegister, useForm } from "react-hook-form";
+import {
+  FieldValues,
+  SubmitHandler,
+  UseFormRegister,
+  useForm,
+} from "react-hook-form";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { useAppDispatch, useAppSelector } from "../useRedux";
 import { AddNewCustomerLeadFormField } from "@/components/leads/fields/Add-customer-lead-fields";
 import { generateAddNewLeadCustomerDetailsValidation } from "@/validation/leadsSchema";
 import { ComponentsType } from "@/components/leads/add/AddNewLeadsData";
-import { useEffect, useMemo } from 'react';
-import { readCustomer, setCustomerDetails } from "@/api/slices/customer/customerSlice";
+import { useEffect, useMemo } from "react";
+import {
+  readCustomer,
+  setCustomerDetails,
+} from "@/api/slices/customer/customerSlice";
 import { createLead, updateLead } from "@/api/slices/lead/leadSlice";
 import { updateQuery } from "@/utils/update-query";
 
@@ -17,15 +25,17 @@ export const useAddNewLeadCustomer = (onHandleNext: Function) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const { loading, error, leadDetails } = useAppSelector((state) => state.lead);
-  const { customer, customerDetails } = useAppSelector((state) => state.customer);
+  const { customer, customerDetails } = useAppSelector(
+    (state) => state.customer
+  );
   useEffect(() => {
-    dispatch(readCustomer({ params: { filter: { paginate: 0 } } }))
-  }, [])
+    dispatch(readCustomer({ params: { filter: { paginate: 0 } } }));
+  }, []);
 
   const onCancel = () => {
-    router.pathname = "/leads"
-    updateQuery(router, router.locale as string)
-  }
+    router.pathname = "/leads";
+    updateQuery(router, router.locale as string);
+  };
   const schema = generateAddNewLeadCustomerDetailsValidation(translate);
 
   const {
@@ -36,28 +46,28 @@ export const useAddNewLeadCustomer = (onHandleNext: Function) => {
     formState: { errors },
     watch,
     setValue,
-    reset
+    reset,
   } = useForm<FieldValues>({
     resolver: yupResolver<FieldValues>(schema),
   });
-  const customerType = watch("customerType")
+  const customerType = watch("customerType");
 
-  const type = watch("type")
+  const type = watch("type");
 
   const onCustomerSelect = (id: string) => {
     if (!id) return;
-    const selectedCustomers = customer.filter((item) => item.id === id)
-    dispatch(setCustomerDetails(selectedCustomers?.length > 0 && selectedCustomers[0]))
+    const selectedCustomers = customer.filter((item) => item.id === id);
+    dispatch(
+      setCustomerDetails(selectedCustomers?.length > 0 && selectedCustomers[0])
+    );
 
     reset({
       ...selectedCustomers[0],
       type: type,
-    })
-
-  }
+    });
+  };
 
   useMemo(() => {
-
     if (leadDetails.id) {
       reset({
         fullName: leadDetails.customerID?.fullName,
@@ -69,26 +79,47 @@ export const useAddNewLeadCustomer = (onHandleNext: Function) => {
         phoneNumber: leadDetails.customerID?.phoneNumber,
         mobileNumber: leadDetails.customerID?.mobileNumber,
         address: leadDetails?.customerID?.address,
-      })
+      });
     }
-  }, [leadDetails.id])
+  }, [leadDetails.id]);
 
-  const fields = AddNewCustomerLeadFormField(register, loading, control, { customerType, type, customer, onCustomerSelect, customerDetails, onCancel, leadDetails }, setValue);
-
+  const fields = AddNewCustomerLeadFormField(
+    register,
+    loading,
+    control,
+    {
+      customerType,
+      type,
+      customer,
+      onCustomerSelect,
+      customerDetails,
+      onCancel,
+      leadDetails,
+    },
+    setValue
+  );
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     if (leadDetails?.id) {
-      const apiData = { ...data, step: 1, leadId: leadDetails?.id, stage: ComponentsType.addressEdit }
+      const apiData = {
+        ...data,
+        step: 1,
+        leadId: leadDetails?.id,
+        stage: ComponentsType.addressAdd,
+      };
 
-      const res = await dispatch(createLead({ data: apiData, router, setError, translate }));
-      if (res?.payload) onHandleNext(ComponentsType.addressEdit);
+      const res = await dispatch(
+        createLead({ data: apiData, router, setError, translate })
+      );
+      if (res?.payload) onHandleNext(ComponentsType.addressAdd);
     } else {
-      const apiData = { ...data, step: 1, stage: ComponentsType.addressEdit }
+      const apiData = { ...data, step: 1, stage: ComponentsType.addressAdd };
 
-      const res = await dispatch(createLead({ data: apiData, router, setError, translate }));
-      if (res?.payload) onHandleNext(ComponentsType.addressEdit);
+      const res = await dispatch(
+        createLead({ data: apiData, router, setError, translate })
+      );
+      if (res?.payload) onHandleNext(ComponentsType.addressAdd);
     }
-
   };
   return {
     fields,
@@ -97,6 +128,6 @@ export const useAddNewLeadCustomer = (onHandleNext: Function) => {
     handleSubmit,
     errors,
     error,
-    translate
+    translate,
   };
 };
