e70ca3e7fb9c11e3a14d89a1fa6a5fc5c3b839e6
Talha Nazir R <talha@cloudmeshsolutions.com>
2023-12-21 17:12:07 +0000

    fix

--- src/hooks/invoice/useInvoiceDetail.tsx
+++ src/hooks/invoice/useInvoiceDetail.tsx
@@ -255,6 +255,7 @@ export default function useInvoiceDetail() {
         setIsSendEmail,
         isSendEmail,
         onNextHandle,
+        handleRecurringInvoiceEdit,
         loading,
     }
 }
