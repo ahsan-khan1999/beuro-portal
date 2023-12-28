39dfe5ea1ce2364329e8577bba3c87de8f692ad1
Mateen Nawaz <mateen.nawaz@cloudmeshsolutions.com>
2023-12-28 08:35:07 +0000

    date picker remove fixed

--- src/components/offers/add/fields/add-offer-details-fields.tsx
+++ src/components/offers/add/fields/add-offer-details-fields.tsx
@@ -478,7 +478,7 @@ export const generateDateChildren = (
 
       field: {
         type: Field.div,
-        className: "flex flex-wrap gap-x-3 gap-y-3 mb-3 items-center",
+        className: "grid grid-cols-2 gap-x-3",
         id: `date`,
         children: [
           {
