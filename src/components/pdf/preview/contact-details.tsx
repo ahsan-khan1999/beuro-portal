0483945b93f6c5b47164ec70df8bd5c9a638564b
Ahsan Khan <ahsan.ak943@gmail.com>
2024-02-02 17:33:47 +0000

    offerPdf phoneNumber check added for null & city undefined

--- src/components/pdf/preview/contact-details.tsx
+++ src/components/pdf/preview/contact-details.tsx
@@ -14,7 +14,7 @@ export const ContactDetails = ({
   return (
     <div className="grid grid-cols-4 items-center mt-5 mb-[37px]">
       <div className="flex flex-col gap-[4px] col-span-3">
-        <span className="text-[#000] text-base font-medium">{GenderLabel[gender as keyof typeof GenderLabel]} {name}</span>
+        <span className="text-[#000] text-base font-medium">{GenderLabel[gender as keyof typeof GenderLabel] || ""} {name}</span>
         <span className="text-[#000] text-base font-medium">
           {streetWithNumber}
         </span>
@@ -30,12 +30,15 @@ export const ContactDetails = ({
           </span>
           <span className="text-[#000] text-base font-medium"> {email}</span>
         </div>
-        <div className="space-x-3">
-          <span className="text-[#404040] text-base font-medium">
-            {translation("pdf.phone")}:
-          </span>
-          <span className="text-[#000] text-base font-medium"> {phone}</span>
-        </div>
+        {
+          (phone !== "+" && phone) &&
+          <div className="space-x-3">
+            <span className="text-[#404040] text-base font-medium">
+              {translation("pdf.phone")}:
+            </span>
+            <span className="text-[#000] text-base font-medium"> {phone}</span>
+          </div>
+        }
       </div>
     </div>
   );
