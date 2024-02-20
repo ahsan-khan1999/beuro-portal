6fa2c16a815e662a792fec66398c75c5de922c5a
Ahsan Khan <ahsan.ak943@gmail.com>
2024-02-20 14:15:51 +0000

    tablw row click in offer/contract

--- src/components/contract/table/TableRows.tsx
+++ src/components/contract/table/TableRows.tsx
@@ -57,12 +57,12 @@ const TableRows = ({
         return (
           <div
             key={index}
-            // onClick={() =>
-            //   router.push({
-            //     pathname: "/contract/pdf-preview",
-            //     query: { offerID: item?.id, isMail: true },
-            //   })
-            // }
+            onClick={() =>
+              router.push({
+                pathname: "/contract/pdf-preview",
+                query: { offerID: item?.id, isMail: true },
+              })
+            }
             className="cursor-pointer hover:bg-[#E9E1FF] items-center bg-white px-6  shadow-tableRow xs:w-fit xlg:w-auto mlg:w-full grid xs:grid-cols-[minmax(100px,_100px)_minmax(250px,_4fr)_minmax(300px,_3fr)_minmax(150px,_150px)_minmax(140px,_140px)_minmax(120px,_120px)_minmax(120px,_120px)_minmax(150px,_150px)_minmax(100px,_100px)_minmax(100px,_100px)_minmax(80px,_80px)_minmax(70px,_70px)_minmax(90px,_90px)] mlg:grid-cols-[minmax(70px,_70px),minmax(90px,_3fr)_minmax(80px,_80px)_minmax(80px,_80px)_minmax(140px,_140px)_minmax(70px,_70px)_minmax(60px,_60px)_minmax(60px,_60px)_minmax(50px,_50px)_minmax(90px,_90px)] xlg:grid-cols-[minmax(70px,_70px),minmax(90px,_3fr)_minmax(110px,_110px)_minmax(80px,_80px)_minmax(85px,_85px)_minmax(140px,_140px)_minmax(80px,_80px)_minmax(50px,_50px)_minmax(60px,_60px)_minmax(50px,_50px)_minmax(90px,_90px)] maxSize:grid-cols-[minmax(70px,_70px)_minmax(100px,_3fr)_minmax(120px,_120px)_minmax(100px,_100px)_minmax(100px,_100px)_minmax(140px,_140px)_minmax(80px,_80px)_minmax(50px,_50px)_minmax(60px,_60px)_minmax(60px,_60px)_minmax(90px,_90px)] xMaxSize:grid-cols-[minmax(70px,_70px)_minmax(90px,_4fr)_minmax(120px,_3fr)_minmax(120px,_120px)_minmax(90px,_90px)_minmax(90px,_90px)_minmax(140px,_140px)_minmax(80px,_80px)_minmax(80px,_80px)_minmax(60px,_60px)_minmax(60px,_60px)_minmax(90px,_90px)] xLarge:grid-cols-[minmax(70px,_70px)_minmax(80px,_4fr)_minmax(120px,_3fr)_minmax(110px,_110px)_minmax(100px,_100px)_minmax(90px,_90px)_minmax(90px,_90px)_minmax(140px,_140px)_minmax(80px,_80px)_minmax(60px,_60px)_minmax(60px,_60px)_minmax(60px,_60px)_minmax(90px,_90px)] mt-2 rounded-md"
           >
             <span className="py-4 truncate">{item.contractNumber}</span>
