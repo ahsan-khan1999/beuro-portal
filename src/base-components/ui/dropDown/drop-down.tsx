686acd0e6fbc32ce3a4c2e031e0527185ba2c218
Ahsan Khan <ahsan.ak943@gmail.com>
2024-03-07 20:45:46 +0000

    text Color change

--- src/base-components/ui/dropDown/drop-down.tsx
+++ src/base-components/ui/dropDown/drop-down.tsx
@@ -41,9 +41,7 @@ export const DropDown = ({
     "flex items-center bg-white px-3 py-[10px] w-full min-h-8 border border-lightGray rounded-lg";
   const buttonClasses = combineClasses(defaultClasses, dropDownClassName);
   const textClasses = combineClasses(
-    `text-sm font-medium text-white ${
-      dropDownDisabled ? "text-lightGray" : ""
-    }`,
+    `text-sm font-medium`,
     dropDownTextClassName
   );
 
