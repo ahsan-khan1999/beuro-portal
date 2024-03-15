214f44f3db6c2f22689d8c87aa861ecfb0172b73
Mateen Nawaz <mateen.nawaz@cloudmeshsolutions.com>
2024-03-15 10:08:21 +0000

    translation changes

--- src/hooks/leads/useLeads.tsx
+++ src/hooks/leads/useLeads.tsx
@@ -246,6 +246,69 @@ const useLeads = () => {
     }
   }, [query]);
 
+  // useEffect(() => {
+  //   const parsedPage = parseInt(query.page as string, 10);
+  //   let resetPage = null;
+  //   if (!isNaN(parsedPage)) {
+  //     setCurrentPage(parsedPage);
+  //   } else {
+  //     resetPage = 1;
+  //     setCurrentPage(1);
+  //   }
+
+  //   const queryStatus = query?.status;
+  //   const searchQuery = query?.text as string;
+  //   const sortedValue = query?.sort as string;
+  //   const extraFiltered = query?.date as string;
+
+  //   const queryParams =
+  //     queryStatus || searchQuery || sortedValue || extraFiltered;
+
+  //   if (queryParams !== undefined) {
+  //     const filteredStatus =
+  //       query?.status === "None"
+  //         ? "None"
+  //         : queryParams
+  //             .toString()
+  //             .split(",")
+  //             .filter((item) => item !== "None");
+
+  //     let updatedFilter: {
+  //       status: string | string[];
+  //       text?: string;
+  //       sort?: string;
+  //       date?: {
+  //         $gte?: string;
+  //         $lte?: string;
+  //       };
+  //     } = {
+  //       status: filteredStatus,
+  //     };
+
+  //     if (searchQuery || sortedValue || extraFiltered) {
+  //       updatedFilter.text = searchQuery;
+  //       updatedFilter.sort = sortedValue;
+
+  //       // Parse extraFiltered into an object with $gte and $lte properties
+  //       const dateObj = JSON.parse(extraFiltered);
+  //       updatedFilter.date = dateObj;
+  //     }
+
+  //     setFilter(updatedFilter);
+
+  //     dispatch(
+  //       readLead({
+  //         params: {
+  //           filter: updatedFilter,
+  //           page: (Number(parsedPage) || resetPage) ?? currentPage,
+  //           size: 10,
+  //         },
+  //       })
+  //     ).then((response: any) => {
+  //       if (response?.payload) setCurrentPageRows(response?.payload?.Lead);
+  //     });
+  //   }
+  // }, [query]);
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
