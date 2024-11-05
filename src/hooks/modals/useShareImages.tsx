9f3080592d73b15f07ef35c98ae7e7bcace758e5
itsmushii <mushtaq.ahmad@cloudmeshsolutions.com>
2024-11-05 17:41:28 +0500

    fix: notes icon fixed, modal tab issue fixed

--- src/hooks/modals/useShareImages.tsx
+++ src/hooks/modals/useShareImages.tsx
@@ -23,16 +23,18 @@ export const useShareImages = () => {
 
   useEffect(() => {
     if (images) {
-      if (images && images?.images?.length > 0) {
-        setActiveTab("img_tab");
-      } else if (images?.videos && images?.videos?.length > 0) {
-        setActiveTab("video_tab");
-      } else if (images?.attachments && images?.attachments?.length > 0) {
-        setActiveTab("attachement_tab");
-      } else if (images?.links && images?.links?.length > 0) {
-        setActiveTab("link_tab");
-      } else {
-        setActiveTab(attachementTabs[0]);
+      if (activeTab == "img_tab") {
+        if (images && images?.images?.length > 0) {
+          setActiveTab("img_tab");
+        } else if (images?.videos && images?.videos?.length > 0) {
+          setActiveTab("video_tab");
+        } else if (images?.attachments && images?.attachments?.length > 0) {
+          setActiveTab("attachement_tab");
+        } else if (images?.links && images?.links?.length > 0) {
+          setActiveTab("link_tab");
+        } else {
+          setActiveTab(attachementTabs[0]);
+        }
       }
     }
   }, [images]);
--- src/hooks/modals/useUploadImage.tsx
+++ src/hooks/modals/useUploadImage.tsx
@@ -158,16 +158,18 @@ export const useUploadImage = (
     if (enteredLinks) {
       const { images, links, attachements, video } = enteredLinks;
 
-      if (images && images.length > 0) {
-        setActiveTab("img_tab");
-      } else if (video && video.length > 0) {
-        setActiveTab("video_tab");
-      } else if (attachements && attachements.length > 0) {
-        setActiveTab("attachement_tab");
-      } else if (links && links.length > 0) {
-        setActiveTab("link_tab");
-      } else {
-        setActiveTab(attachementTabs[0]);
+      if (activeTab == "img_tab") {
+        if (images && images.length > 0) {
+          setActiveTab("img_tab");
+        } else if (video && video.length > 0) {
+          setActiveTab("video_tab");
+        } else if (attachements && attachements.length > 0) {
+          setActiveTab("attachement_tab");
+        } else if (links && links.length > 0) {
+          setActiveTab("link_tab");
+        } else {
+          setActiveTab(attachementTabs[0]);
+        }
       }
     }
   }, [enteredLinks]);
