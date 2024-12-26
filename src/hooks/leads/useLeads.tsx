f0f58df64655e79d8a11287262a4ce126e3bfcb9
Mateen Nawaz <mateen.nawaz@cloudmeshsolutions.com>
2024-12-26 18:36:47 +0500

    fix: some design issues

--- src/hooks/leads/useLeads.tsx
+++ src/hooks/leads/useLeads.tsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { useAppSelector } from "../useRedux";
 import { updateModalType } from "@/api/slices/globalSlice/global";
 import { ModalConfigType, ModalType } from "@/enums/ui";
-import { Lead, LeadsFilterProps } from "@/types/leads";
+import { Lead } from "@/types/leads";
 import ExistingNotes from "@/base-components/ui/modals1/ExistingNotes";
 import AddNewNote from "@/base-components/ui/modals1/AddNewNote";
 import { DEFAULT_CUSTOMER, DEFAULT_LEAD, staticEnums } from "@/utils/static";
@@ -17,7 +17,7 @@ import {
 import localStoreUtil from "@/utils/localstore.util";
 import { useRouter } from "next/router";
 import { deleteNotes, readNotes } from "@/api/slices/noteSlice/noteSlice";
-import { readImage, setImages } from "@/api/slices/imageSlice/image";
+import { readImage } from "@/api/slices/imageSlice/image";
 import { setCustomerDetails } from "@/api/slices/customer/customerSlice";
 import { FiltersDefaultValues } from "@/enums/static";
 import { useTranslation } from "next-i18next";
@@ -185,7 +185,7 @@ const useLeads = () => {
   }, []);
 
   const onClose = () => {
-    dispatch(updateModalType(ModalType.NONE));
+    // dispatch(updateModalType(ModalType.NONE));
 
     dispatch(updateModalType({ type: ModalType.NONE }));
     if (noteInfo.id) {
